303380d769f1578d971de4abc8f27f17e724e05f
Benjamin Glaspie <02Benjamin.Glaspie@gulllakecs.org>
2019-03-13 07:07:11 -0700

    Add new code

--- Final code project.py
+++ Final code project.py
@@ -1,13 +1,12 @@
 #Ben Glaspie
-#mad libs
-
-your_name = (input("your name: "))
-city_state_zip_code = (input("city, state, zip code: "))
-representaive_name = (input("Representave name: "))
-new_horrifing_appointment_or_bill = (input("New horrifing appointmant or bill: "))
-pronoun = (input("pronoun: "))
-instant_adjective = (input("instant adjective: "))
-adjective_describing_something_unconstitutional = (input("adjective decribing something unconstitutional: ))
-noun_like_racist_or_just_racist = (input("Noun like racist, or just racist: " ))
-scary_verb = (input("scary verb: "))
-optionl_improvisation_eg_seriously_though_what_is_going_on = (input("Optionl improvisation eg seriously though what is going on"))
+#This is my code it will make a yellow and red star 
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
