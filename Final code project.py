93232925daf2c0c2cd66d0691e354067a223fbd9
Benjamin Glaspie <02Benjamin.Glaspie@gulllakecs.org>
2019-03-13 06:48:35 -0700

    Add new repository

---
+++ Final code project.py
@@ -0,0 +1,13 @@
+#Ben Glaspie
+#mad libs
+
+your_name = (input("your name: "))
+city_state_zip_code = (input("city, state, zip code: "))
+representaive_name = (input("Representave name: "))
+new_horrifing_appointment_or_bill = (input("New horrifing appointmant or bill: "))
+pronoun = (input("pronoun: "))
+instant_adjective = (input("instant adjective: "))
+adjective_describing_something_unconstitutional = (input("adjective decribing something unconstitutional: ))
+noun_like_racist_or_just_racist = (input("Noun like racist, or just racist: " ))
+scary_verb = (input("scary verb: "))
+optionl_improvisation_eg_seriously_though_what_is_going_on = (input("Optionl improvisation eg seriously though what is going on"))
