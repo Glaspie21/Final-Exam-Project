794620f748eb23525a2905ae23155592720c6e7d
Glaspie21 <48022388+Glaspie21@users.noreply.github.com>
2019-03-13 07:17:49 -0700

    Update Final code project.py

--- Final code project.py
+++ Final code project.py
@@ -8,4 +8,12 @@ while True:
     left(170)
     if abs(pos()) < 1:
         break
+end_fill()
+''' now I will ask what they thought about my code 
+using a simple print code '''
 
+rate_out_of_10 = (input("Rate my star out of 10: "))
+thankyou = (input("Thank you for rateing my star!"))
+
+print(rate_out_of_10)
+print(thankyou)
